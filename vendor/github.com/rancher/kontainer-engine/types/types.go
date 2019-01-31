75cc2e36a8ef083318e850826d24365b3547a9fd
gitlawr <lawrleegle@gmail.com>
2019-01-31 15:04:00 +0800

    update vendors

--- vendor/github.com/rancher/kontainer-engine/types/types.go
+++ vendor/github.com/rancher/kontainer-engine/types/types.go
@@ -49,6 +49,12 @@ type Driver interface {
 
 	// Get driver capabilities
 	GetCapabilities(ctx context.Context) (*Capabilities, error)
+
+	// Remove legacy service account token
+	RemoveLegacyServiceAccount(ctx context.Context, clusterInfo *ClusterInfo) error
+
+	ETCDSave(ctx context.Context, clusterInfo *ClusterInfo, opts *DriverOptions, snapshotName string) error
+	ETCDRestore(ctx context.Context, clusterInfo *ClusterInfo, opts *DriverOptions, snapshotName string) error
 	GetK8SCapabilities(ctx context.Context, opts *DriverOptions) (*K8SCapabilities, error)
 }
 
